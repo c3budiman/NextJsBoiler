5f79df771fa21971d3e91ab023b75903e7aa1939
c3budiman <c3budiman@gmail.com>
2020-12-13 18:06:52 +0700

    upgrading nextjs 9 to nextjs 10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import GreenButton from '../components/button/GreenButton'
+import Image from 'next/image'
 
 export default function Index() {
     return (
@@ -8,9 +9,13 @@ export default function Index() {
                 <div className="header-c3budiman">
                     <div className="inside header">
                         <h3>Welcome!</h3>
+                        <div className="my-pictures">
+                            <Image src="/images/cecep.jpg" width="400" height="400" alt="Profile Picture" />
+                        </div>
                         <GreenButton text="Learn More?" type="button" onClick={() => alert('wow you clicked it!')} />
                     </div>
                 </div>
+
             </header>
         </>
     )
